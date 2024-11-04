fc167ac6b1ffcc50e52a15e052f69824382ca9a0
Toma <kirima--idea--@au.com>
2024-11-04 10:54:37 +0900

    [update] 新規予約にエラーハンドリングを追加

--- handler/reservation_handler.go
+++ handler/reservation_handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"errors"
 	"net/http"
 
 	logging "food-shuffle-api/log"
@@ -11,6 +12,7 @@ import (
 	"food-shuffle-api/service"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-sql-driver/mysql"
 )
 
 var ReservationService = service.ReservationService{}
@@ -18,7 +20,7 @@ var ReservationService = service.ReservationService{}
 func ReservationRegistorHandler(ctx *gin.Context) {
 	// ヘッダーのContent-Typeにapplication/jsonが含まれているか確認
 	if ctx.GetHeader("Content-Type") != "application/json" {
-		logging.LogError("Content-Type is not application/json", custom_error.NewError(custom_error.InvalidDataError))
+		logging.LogError("Content-Type is not application/json", nil)
 
 		// エラーレスポンスを返す
 		conversion.ResponseJson(ctx, http.StatusUnsupportedMediaType, nil)
@@ -41,7 +43,25 @@ func ReservationRegistorHandler(ctx *gin.Context) {
 	if err != nil {
 		// エラーログを書き込む
 		logging.LogError("Error logging in:", err)
+		// カスタムエラーのエラー型を宣言
+		var customErr *custom_error.CustomError
+		if errors.As(err, &customErr) {
+			conversion.ResponseJson(ctx, customErr.StatusCode(), nil)
+			return
+		}
+
+		var mysqlErr *mysql.MySQLError
+		if errors.As(err, &customErr) {
+			switch mysqlErr.Number {
+			case 1452:
+				conversion.ResponseJson(ctx, http.StatusBadRequest, nil)
+				return
+			}
+		}
+		conversion.ResponseJson(ctx, http.StatusInternalServerError, nil)
+		return
 	}
+
 	//成功レスポンス
-	conversion.ResponseJson(ctx, http.StatusOK, gin.H{"Token": result})
+	conversion.ResponseJson(ctx, http.StatusOK, gin.H{"ReservationUuid": result})
 }
