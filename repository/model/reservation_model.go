24372783043f0f16b5399a8250344c72d6e9a47f
Toma <kirima--idea--@au.com>
2025-02-02 15:07:46 +0900

    [fix]予約時間が日付のみになっていたので時間もデータベースに登録されるよう修正

--- repository/model/reservation_model.go
+++ repository/model/reservation_model.go
@@ -6,7 +6,7 @@ type Reservation struct {
 	ReservationUuid   string    `gorm:"type:char(36);primary_key"`
 	RestaurantUuid    string    `gorm:"type:char(36);foreignKey:RestaurantUuid"`
 	UserUuid          string    `gorm:"type:char(36);foreignKey:UserUuid"`
-	ReservationDate   time.Time `gorm:"type:date;not null"`
+	ReservationDate   time.Time `gorm:"not null"`
 	NumberOfPeople    int       `gorm:"type:integer;not null"`
 	CourseUuid        *string   `gorm:"type:char(36)"`
 	CampaignUuid      *string   `gorm:"type:char(36)"`
