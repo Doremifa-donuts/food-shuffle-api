5a5e43f2ed7a1b307781f0047553a476c62fde0b
grfsv <umequiqui@gmail.com>
2025-02-07 13:05:40 +0900

    [add] viewerにレビューを受け取らせた

--- repository/model/user_review_flag_model.go
+++ repository/model/user_review_flag_model.go
@@ -45,4 +45,79 @@ var sampleUserReviewFlag = []UserReviewFlag{
 		UserUuid:     "91a78381-f472-496b-90e3-2c66a33391d1",
 		ReviewStatus: Unclassified,
 	},
+	{
+		ReviewUuid:   "019465a5-8670-76be-a2e0-45855c448be2",
+		UserUuid:     "91a78381-f472-496b-90e3-2c66a33391d1",
+		ReviewStatus: Unclassified,
+	},
+	{
+		ReviewUuid:   "0194de87-aed4-7972-be26-13d18d58c9b2", // らーめん大王
+		UserUuid:     "91a78381-f472-496b-90e3-2c66a33391d1",
+		ReviewStatus: Unclassified,
+	},
+	// {
+	// 	ReviewUuid:   "0194656b-cd71-7775-b489-a8d37ca623a7", // おにぎり
+	// 	UserUuid:     "91a78381-f472-496b-90e3-2c66a33391d1",
+	// 	ReviewStatus: Unclassified,
+	// },
+	{
+		ReviewUuid:   "0194de89-3d75-7200-81db-dca8a2969c1e", // ビストロ
+		UserUuid:     "91a78381-f472-496b-90e3-2c66a33391d1",
+		ReviewStatus: Unclassified,
+	},
+	{
+		ReviewUuid:   "0194de76-c2f2-71f6-9c4c-0fd397047142", // らーめん大王
+		UserUuid:     "91a78381-f472-496b-90e3-2c66a33391d1",
+		ReviewStatus: Unclassified,
+	},
+	{
+		ReviewUuid:   "0194de76-fc3c-72ed-93a5-5f4ed033d91f", // おにぎり
+		UserUuid:     "91a78381-f472-496b-90e3-2c66a33391d1",
+		ReviewStatus: Unclassified,
+	},
+	{
+		ReviewUuid:   "0194de77-2bbe-7d16-8214-7a89cc41f597", // ビストロ
+		UserUuid:     "91a78381-f472-496b-90e3-2c66a33391d1",
+		ReviewStatus: Unclassified,
+	},
+	{
+		ReviewUuid:   "0194de77-5c1d-7877-8ebb-436e8a16c1a8", // らーめん大王
+		UserUuid:     "91a78381-f472-496b-90e3-2c66a33391d1",
+		ReviewStatus: Unclassified,
+	},
+	{
+		ReviewUuid:   "0194de77-f924-7698-91aa-6e4146b0cdd6", // おにぎり
+		UserUuid:     "91a78381-f472-496b-90e3-2c66a33391d1",
+		ReviewStatus: Unclassified,
+	},
+	{
+		ReviewUuid:   "0194de78-2b3b-793d-8006-86aabff0306c", // ビストロ
+		UserUuid:     "91a78381-f472-496b-90e3-2c66a33391d1",
+		ReviewStatus: Unclassified,
+	},
+	{
+		ReviewUuid:   "0194de78-671c-7b6b-a84e-0e6e9055f9a7", // らーめん大王
+		UserUuid:     "91a78381-f472-496b-90e3-2c66a33391d1",
+		ReviewStatus: Unclassified,
+	},
+	// {
+	// 	ReviewUuid:   "0194de78-671c-7b6b-a84e-0e6e9055f9a7", // らーめん大王
+	// 	UserUuid:     "91a78381-f472-496b-90e3-2c66a33391d1",
+	// 	ReviewStatus: Unclassified,
+	// },
+	{
+		ReviewUuid:   "0194de78-8e8c-7949-b2b0-8ce79a2b11cb", // らーめん大王
+		UserUuid:     "91a78381-f472-496b-90e3-2c66a33391d1",
+		ReviewStatus: Unclassified,
+	},
+	{
+		ReviewUuid:   "0194de78-c7ca-776e-8ec3-1ec8c08fb961", // おにぎり
+		UserUuid:     "91a78381-f472-496b-90e3-2c66a33391d1",
+		ReviewStatus: Unclassified,
+	},
+	{
+		ReviewUuid:   "0194de78-ed4c-76b5-9d00-84e60a5fdd21", // ビストロ
+		UserUuid:     "91a78381-f472-496b-90e3-2c66a33391d1",
+		ReviewStatus: Unclassified,
+	},
 }
